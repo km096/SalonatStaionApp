cc5330c052b159eea548551433f57b152cf70b9f
Kareem Khaled <kareem.khaled1000@gmail.com>
2023-10-15 22:52:39 +0300

    Initial Commit

---
+++ SalonatStationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SalonatStationApp
+//
+//  Created by ME-MAC on 10/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
